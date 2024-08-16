43c0ec8c3ceb54b7c78448a68353fbc1d72208fa
Remone Carter <remonecarter1025@icloud.com>
2024-08-16 12:42:43 -0400

    Initial Commit

---
+++ Scholarlly/ScholarllyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScholarllyApp.swift
+//  Scholarlly
+//
+//  Created by student on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ScholarllyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
